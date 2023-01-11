c162877daf44465338b8ce0d12a6fbcf97e6aaf2
“VIOLET-BOSIRE” <violet.bosire@student.moringaschool.com>
2023-01-11 15:11:22 +0300

    changes on src

---
+++ src/component/Footer.js
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <section>
+            <h2>this is a footer</h2>
+        </section>
+    )
+}
+export default Footer;
---
+++ src/component/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar(){
+    return(
+        <div>
+            <h1> this is a navbar</h1>
+        </div>
+    )
+
+}
+export default Navbar;
